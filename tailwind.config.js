a2c46424b566673f28332d22585a8fea852eb662
yeasin2448 <ayeasin108@gmail.com>
2023-06-14 17:34:29 +0600

    1st update

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,12 +30,14 @@ module.exports = {
         primary_color: "#0DC1D9",
         text_color: "#5e6e82",
         border_color: "#344050",
+        th_border: "#d8e2ef",
         text_colors: "#344050",
       },
       boxShadow: {
         theme_shadow: "0px 0px 15px rgba(46, 49, 146, 0.06);",
         primary_shadow:
           "0 7px 14px 0 rgba(65,69,88,.1), 0 3px 6px 0 rgba(0,0,0,.07)",
+        after_shadow: "0 0.125rem 0.25rem rgba(0,0,0,.075)",
       },
     },
     fontFamily: {
